716e571204e4d17efc125a08d75c4b7b4b0a555a
Joe Booth <joseph.booth3@gmail.com>
2018-05-16 15:13:33 -0500

    setting up modules

--- liri.js
+++ liri.js
@@ -1,1 +1,28 @@
 require("dotenv").config();
+
+//setting up modules
+var keys = require('./keys.js');
+var request = require('request');
+var spotify = require('spotify');
+var Twitter = require('twitter');
+var client = new twitter(keys.twitter);
+var fs = require('fs');
+
+//store argument 
+var nodeArgv = process.argv;
+var command = process.argv[2];
+
+
+//switch case for command user gives
+
+
+
+//function for displaying song
+
+
+//function for displaying movie
+
+
+//function for displaying tweets
+
+
