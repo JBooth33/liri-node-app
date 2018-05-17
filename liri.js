3d2ff1f208a1f20d1d6c2a50bd2627ee7ef7610f
Joe Booth <joseph.booth3@gmail.com>
2018-05-17 11:10:00 -0500

    adding switch statement for user input

--- liri.js
+++ liri.js
@@ -14,14 +14,26 @@ var command = process.argv[2];
 
 
 //switch case for command user gives
-
+switch(command) {
+  case "spotify-this-song":
+    getSongInfo();
+    break;
+  case "my-tweets":
+    getTweets();
+    break;
+  case "movie-this":
+    getMovieInfo();
+    break;
+  case "do-what-it-says":
+    doSomething();
+    break;
+  default:
+    console.log("Type one of the following commands after 'node liri.js' : spotify-this-song + song name, my-tweets, movie-this + movie name, do-whatever-it-says")
+}
 
 
 //function for displaying song
-var spotify = new Spotify({
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-});
+/*var spotify = new Spotify(keys.spotify);
  
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
   if (err) {
@@ -30,15 +42,23 @@ spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, d
  
 console.log(data); 
 });
-
+*/
 
 
 //function for displaying tweets
-
+/*
 var params = {screen_name: 'javy23baez'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
   }
 });
+*/
 
+//function for displaying movie info
+function getMovieInfo (movie) {
+  var omdbURL = "http://www.omdbapi.com/?apikey=" + process.env.OMDB_KEY + "&t=" + movie + "&plot=short&tomatoes=true";
+  console.log (omdbURL);
+}
+
+getMovieInfo('It');
