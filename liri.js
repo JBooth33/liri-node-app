7397886ade5832d1eca75dcb35b4612bea0d75f6
Joe Booth <joseph.booth3@gmail.com>
2018-05-17 08:54:47 -0500

    setting up successful request to Spotify and Twitter API

--- liri.js
+++ liri.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 //setting up modules
 var keys = require('./keys.js');
 var request = require('request');
-var spotify = require('spotify');
+var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
-var client = new twitter(keys.twitter);
+var client = new Twitter(keys.twitter);
 var fs = require('fs');
 
 //store argument 
@@ -18,11 +18,27 @@ var command = process.argv[2];
 
 
 //function for displaying song
+var spotify = new Spotify({
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
 
-
-//function for displaying movie
 
 
 //function for displaying tweets
 
+var params = {screen_name: 'javy23baez'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
 
