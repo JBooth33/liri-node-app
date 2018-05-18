e24be4062a24ec6f7c7e00ac50abcf0781ec172f
Joe Booth <joseph.booth3@gmail.com>
2018-05-18 16:40:17 -0500

    adding functions for if user does not enter a song or movie

--- liri.js
+++ liri.js
@@ -16,13 +16,21 @@ var userChoice = process.argv[3];
 //switch case for command user gives
 switch(command) {
   case "spotify-this-song":
-    getSongInfo(userChoice);
+    if(userChoice){
+      getSongInfo(userChoice);
+    } else {
+      thatWay();
+    }
     break;
   case "my-tweets":
     getTweets();
     break;
   case "movie-this":
-    getMovieInfo(userChoice);
+    if(userChoice) {
+      getMovieInfo(userChoice);
+    } else {
+      mrNobody();
+    }
     break;
   case "do-what-it-says":
     doSomething();
@@ -45,11 +53,10 @@ spotify.search({ type: 'track', query: userChoice, limit: 1}, function(err, data
   if (err) {
     return console.log('Error occurred: ' + err);
   }
- 
   var results = data.tracks.items[0];
   console.log("Artist: " + results.artists[0].name);
   console.log("Song: " + results.name);
-  console.log("songURL: " + results.external_urls.spotify);
+  console.log("Song URL: " + results.external_urls.spotify);
   console.log("Album: " + results.album.name);
 });
 
@@ -62,12 +69,12 @@ spotify.search({ type: 'track', query: userChoice, limit: 1}, function(err, data
 //function for displaying tweets
 
 function getTweets() {
-  var params = {screen_name: 'javy23baez'};
+  var params = {screen_name: 'JBooth_33'};
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
       for(var i = 0; i < 20; i++) {
         var date = tweets[i].created_at;
-        console.log("@javy23baez: " + tweets[i].text + " Tweeted On: " + date.substring(0, 19));
+        console.log("@JBooth_33: " + tweets[i].text + " Tweeted On: " + date.substring(0, 19));
       }
     }
   });
@@ -81,6 +88,7 @@ function getMovieInfo () {
   var omdbURL = "http://www.omdbapi.com/?apikey=" + process.env.OMDB_KEY + "&t=" + movie + "&plot=short&tomatoes=true";
   console.log (omdbURL);
   request(omdbURL, function(error, response, body) {
+    
     if (!error && response.statusCode === 200) {
       console.log("Title: " + JSON.parse(body).Title);
       console.log("Release Year: " + JSON.parse(body).Year);
@@ -90,7 +98,46 @@ function getMovieInfo () {
       console.log("Language: " + JSON.parse(body).Language);
       console.log("Plot: " + JSON.parse(body).Plot);
       console.log("Actors: " + JSON.parse(body).Actors);
-    }
+    } 
   });
 }
 
+function thatWay() {
+  var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+});
+ 
+spotify.search({ type: 'track', query: "I-want-it-that-way", limit: 1}, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+  var results = data.tracks.items[0];
+
+  console.log("Artist: " + results.artists[0].name);
+  console.log("Song: " + results.name);
+  console.log("Song URL: " + results.external_urls.spotify);
+  console.log("Album: " + results.album.name);
+});
+}
+
+function mrNobody() {
+  var movie = "mr.-nobody";
+  var omdbURL = "http://www.omdbapi.com/?apikey=" + process.env.OMDB_KEY + "&t=" + movie + "&plot=short&tomatoes=true";
+  console.log (omdbURL);
+  request(omdbURL, function(error, response, body) {
+    
+    if (!error && response.statusCode === 200) {
+      console.log("Title: " + JSON.parse(body).Title);
+      console.log("Release Year: " + JSON.parse(body).Year);
+      console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+      console.log("Country Where Movie Produced: " + JSON.parse(body).Country);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+    } 
+  });
+}
